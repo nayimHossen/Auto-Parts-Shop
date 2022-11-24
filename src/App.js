5dd1e64fd2aaed0dae6fc24a3403d75eb8b97386
nayim-hossen <nayimh2017@gmail.com>
2022-11-25 04:20:37 +0600

    folder setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/header/Navbar";
+import About from "./pages/about/About";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 function App() {
   return (
     <div>
-      <h2>Home page</h2>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="blogs" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
