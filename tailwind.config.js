d0877a54e754e6df8ef852507a3ad9f9c3e2819b
nayim-hossen <nayimh2017@gmail.com>
2022-11-25 03:58:22 +0600

    ready

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "blue",
+
+          secondary: "#355FD9",
+
+          accent: "#0E254E",
+
+          neutral: "#BECDCE",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+  //...
+};
